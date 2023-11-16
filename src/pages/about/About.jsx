53243ddedd7693caa313720a3039407869d7d8fc
bahare <pondblue07@gmail.com>
2023-11-17 00:58:14 +0330

    imported navbar to about and addArticle components

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,6 +1,11 @@
+import MyNavbar from "../../components/navbar/MyNavbar"
+
 function About(){
   return(
+    <>
+    <MyNavbar />
     <h1>About page</h1>
+    </>
   )
 }
 export default About
--- src/pages/addArticle/AddArticle.jsx
+++ src/pages/addArticle/AddArticle.jsx
@@ -1,6 +1,11 @@
+import MyNavbar from "../../components/navbar/MyNavbar"
+
 function AddArticle(){
   return(
+    <>
+    <MyNavbar />
     <h1>AddArticle page</h1>
+    </>
   )
 }
 export default AddArticle
